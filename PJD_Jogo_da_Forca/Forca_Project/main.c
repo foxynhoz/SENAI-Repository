7fcd96336b0131ce3cc0f3c2369f2d2adb3cd9bc
Foxyz <waterfox24699@gmail.com>
2025-05-08 02:02:33 -0300

    Adicionado tentativas

--- PJD_Jogo_da_Forca/Forca_Project/main.c
+++ PJD_Jogo_da_Forca/Forca_Project/main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 int main()
 {
+    bool gotOne;
     char palavra[] = "daniel";
     char resposta[strlen(palavra)];
     char usrPalpite;
+    int tentativas = 6;
 
      for(int i = 0; i < strlen(palavra); i++)
     {
@@ -14,16 +17,20 @@ int main()
     }
     while(1)
     {
+        gotOne = false;
         printf("%s \n", resposta);
-        printf("\n\nDigite um letra:\n");
+        printf("\n\nDigite um letra Tentativas: %d:\n",tentativas);
         scanf(" %c", &usrPalpite);
         for(int i = 0; i < strlen(palavra); i++)
         {
             if(usrPalpite == palavra[i])
             {
-                resposta[i]=palavra[i];
+                gotOne = true;
+                resposta[i] = palavra[i];
             }
-
+        }
+        if(gotOne == false){
+            tentativas--;
         }
     }
 return 0;
