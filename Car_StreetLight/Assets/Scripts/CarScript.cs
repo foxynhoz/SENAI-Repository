3671f1faa18ddec4a662f0f7dac8e19d72bf8973
foxynhoz <waterfox24699@gmail.com>
2025-12-06 19:28:08 -0300

    Car Collision added

--- Car_StreetLight/Assets/Scripts/CarScript.cs
+++ Car_StreetLight/Assets/Scripts/CarScript.cs
@@ -5,12 +5,16 @@ public class CarScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        Debug.Log("Semas");
     }
 }
